3d650917d1155b4149fbcfd8ff57c34b21c28883
Elijah <elijaherap3@gmail.com>
2020-05-22 19:23:39 -0700

    js for api call and search form

--- assets/index.js
+++ assets/index.js
@@ -10,14 +10,34 @@ var formSubmitHandler = function(event) {
     var city = cityInputEl.value.trim();
 
     if (city) {
-        getUserRepos(username);
+        getCityWeather(city);
         cityInputEl.value = "";
     } else {
         alert("Please enter a City");
     }  
 };
-// api fetch call function for city's weather conditions
 
+// api fetch call function for city's weather conditions
+var getCityWeather = function(city) {
+    // format the github api url
+    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7b426fc3500aff841c7ff77e32992af5";
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+            response.json().then(function(data) {
+                console,log(name);
+            });
+            } else {
+            alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            // Notice this '.catch()' getting chained onto the end of the '.then()' method
+            alert("Unable to connect to OpenWeatherMap.org");
+        });
+};
 // render current weather conditions
 
 // render 5 day forecast
